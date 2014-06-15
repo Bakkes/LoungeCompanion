15ce6e6b064b0cd3840beded70902ee1b67f4845
U-chris-pc\Chris <chris.m@live.nl>
2014-06-15 15:15:55 +0200

    Beautified background.js

--- js/background.js
+++ js/background.js
@@ -1,55 +1,58 @@
-String.prototype.endsWith = function(suffix) {
+String.prototype.endsWith = function (suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
 
-chrome.notifications.onClicked.addListener(function(id, index) {
-	if(id.endsWith("-trade")) {
-		chrome.tabs.create({ url: "http://csgolounge.com/mytrades" });
-	} else if(id.endsWith("-offer")) {
-		chrome.tabs.create({ url: "http://csgolounge.com/myoffers" });
-	}
+chrome.notifications.onClicked.addListener(function (id, index) {
+    if (id.endsWith("-trade")) {
+        chrome.tabs.create({
+            url: "http://csgolounge.com/mytrades"
+        });
+    } else if (id.endsWith("-offer")) {
+        chrome.tabs.create({
+            url: "http://csgolounge.com/myoffers"
+        });
+    }
 });
 
 var notificationId = 1337;
+
 function checkForNotifications() {
-	$.get("http://csgolounge.com/", function(data){
-		data = $(data);
-		var trades = data.find('a[href$="mytrades"]:first');
-		var offers = data.find('a[href$="myoffers"]:first');
-		
-		if(trades.find(".notification").length > 0) {
-			var notification = trades.find(".notification");
-			var newCount = notification.text();
-			
-			chrome.notifications.create(notificationId + "-trade", 
-				{
-					type: "basic",
-					title: "Trade update",
-					iconUrl: "http://csgolounge.com/img/my_trades.png",
-					isClickable: true,
-					message: newCount == 1 ? "You have 1 new comment on your trades" : "You have " + newCount + " new comments on your trades"
-				}, function(){
-					notificationId++;
-				});
-		}
-		if(offers.find(".notification").length > 0) {
-			var notification = offers.find(".notification");
-			var newCount = notification.text();
-			
-			chrome.notifications.create(notificationId + "-offer", 
-				{
-					type: "basic",
-					title: "Offer update",
-					isClickable: true,
-					iconUrl: "http://csgolounge.com/img/my_offers.png",
-					message: newCount == 1 ? "You have 1 new comment on your offers" : "You have " + newCount + " new comments on your offers"
-				}, function(){
-					notificationId++;
-				});
-		}
-		
-	});
-	setTimeout(checkForNotifications, 15000);
+    $.get("http://csgolounge.com/", function (data) {
+        data = $(data);
+        var trades = data.find('a[href$="mytrades"]:first');
+        var offers = data.find('a[href$="myoffers"]:first');
+
+        if (trades.find(".notification").length > 0) {
+            var notification = trades.find(".notification");
+            var newCount = notification.text();
+
+            chrome.notifications.create(notificationId + "-trade", {
+                type: "basic",
+                title: "Trade update",
+                iconUrl: "http://csgolounge.com/img/my_trades.png",
+                isClickable: true,
+                message: newCount == 1 ? "You have 1 new comment on your trades" : "You have " + newCount + " new comments on your trades"
+            }, function () {
+                notificationId++;
+            });
+        }
+        if (offers.find(".notification").length > 0) {
+            var notification = offers.find(".notification");
+            var newCount = notification.text();
+
+            chrome.notifications.create(notificationId + "-offer", {
+                type: "basic",
+                title: "Offer update",
+                isClickable: true,
+                iconUrl: "http://csgolounge.com/img/my_offers.png",
+                message: newCount == 1 ? "You have 1 new comment on your offers" : "You have " + newCount + " new comments on your offers"
+            }, function () {
+                notificationId++;
+            });
+        }
+
+    });
+    setTimeout(checkForNotifications, 15000);
 }
 
 checkForNotifications();
